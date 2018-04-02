15a401dca5d1094e39c7f56b83289aec2141e337
Cory Hollinger <cory.hollinger@gmail.com>
2018-04-02 15:50:25 -0500

    added comment back in

--- Modules/CalcDefence-3_0.lua
+++ Modules/CalcDefence-3_0.lua
@@ -382,6 +382,7 @@ function calcs.defence(env, actor)
 				takenMore = takenMore * modDB:Sum("MORE", nil, "ElementalDamageTakenOverTime")
 			end
 			local resist = output[damageType.."Resist"]
+			-- Fix #942 - don't let resists over 100 give degen
 			output[damageType.."TakenDotMult"] = (1 - m_min(resist, 100) / 100) * (1 + takenInc / 100) * takenMore
 			if breakdown then
 				breakdown[damageType.."TakenDotMult"] = { }
