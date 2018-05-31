1a4c95633212a6e897fdae4c5ba450f86d92be48
ExaltedShard <39774255+ExaltedShard@users.noreply.github.com>
2018-05-31 16:52:58 +0900

    Better wording and imply a skill is used

--- Modules/ConfigOptions.lua
+++ Modules/ConfigOptions.lua
@@ -512,8 +512,9 @@ return {
 	{ var = "conditionBlockedHitFromUniqueEnemyInPast10Sec", type = "check", ifVer = "3_0", label = "Blocked hit from a Unique in the past 10s?", ifNode = 63490, apply = function(val, modList, enemyModList)
 		modList:NewMod("Condition:BlockedHitFromUniqueEnemyInPast10Sec", "FLAG", true, "Config", { type = "Condition", var = "Combat" })
 	end },
-	{ var = "multiplierManaUsedRecently", type = "count", label = "# Mana Used Recently:", ifMult = "ManaUsedRecently", tooltip = "This option is specific to Indigon.", apply = function(val, modList, enemyModList)
-		modList:NewMod("Multiplier:ManaUsedRecently", "BASE", val, "Config", { type = "Condition", var = "Combat" })
+	{ var = "multiplierManaSpentRecently", type = "count", label = "# Mana Spent Recently:", ifMult = "ManaSpentRecently", implyCond = "UsedSkillRecently", tooltip = "This option is specific to Indigon.\nThis also implies that you have used a Skill Recently.", apply = function(val, modList, enemyModList)
+		modList:NewMod("Multiplier:ManaSpentRecently", "BASE", val, "Config", { type = "Condition", var = "Combat" })
+		modList:NewMod("Condition:UsedSkillRecently", "FLAG", val >= 1, "Config", { type = "Condition", var = "Combat" })
 	end },
 
 	-- Section: Effective DPS options
