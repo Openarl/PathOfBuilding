672ed490e0df22d41202b0d8a8db49f67eb40def
Quotae <39030429+Quotae@users.noreply.github.com>
2019-09-25 20:54:50 -0500

    Add files via upload
    made self-shock config apply increased damage taken

--- Modules/ConfigOptions.lua
+++ Modules/ConfigOptions.lua
@@ -458,6 +458,7 @@ return {
 	end },
 	{ var = "conditionShocked", type = "check", label = "Are you Shocked?", ifCond = "Shocked", apply = function(val, modList, enemyModList)
 		modList:NewMod("Condition:Shocked", "FLAG", true, "Config", { type = "Condition", var = "Combat" })
+		modList:NewMod("DamageTaken", "INC", 50, "Shock", { type = "Condition", var = "Shocked" })
 	end },
 	{ var = "conditionBleeding", type = "check", label = "Are you Bleeding?", ifCond = "Bleeding", apply = function(val, modList, enemyModList)
 		modList:NewMod("Condition:Bleeding", "FLAG", true, "Config", { type = "Condition", var = "Combat" })
@@ -721,7 +722,7 @@ return {
 	{ var = "conditionEnemyFrozen", type = "check", label = "Is the enemy Frozen?", implyCond = "Chilled", tooltip = "This also implies that the enemy is Chilled.", apply = function(val, modList, enemyModList)
 		enemyModList:NewMod("Condition:Frozen", "FLAG", true, "Config", { type = "Condition", var = "Effective" })
 	end },
-	{ var = "conditionEnemyShocked", type = "check", label = "Is the enemy Shocked?", tooltip = "In addition to allowing any 'against Shocked Enemies' modifiers to apply,\nthis will allow you to input the effect of the Shock applied to the enemy.\nShock increases Damage Taken by the enemy by the Effect of Shock specified.\n\nGuaranteed sources of Shock with an unspecified effect apply a base Shock of 20%.\nBase Shock values are increased by Effect of Shock and Effect of non-Damaging Ailments.", apply = function(val, modList, enemyModList)
+	{ var = "conditionEnemyShocked", type = "check", label = "Is the enemy Shocked?", tooltip = "In addition to allowing any 'against Shocked Enemies' modifiers to apply,\nthis will allow you to input the effect of the Shock applied to the enemy.\nShock increases Damage Taken by the enemy by the Effect of Shock specified,\nup to a maximum effect of 50% increased Damage Taken.\n\nGuaranteed sources of Shock with an unspecified effect apply a base Shock of 20%.\nBase Shock values are increased by Effect of Shock and Effect of non-Damaging Ailments.", apply = function(val, modList, enemyModList)
 		enemyModList:NewMod("Condition:Shocked", "FLAG", true, "Config", { type = "Condition", var = "Effective" })
 	end },
 	{ var = "conditionShockEffect", type = "count", label = "Effect of Shock:", ifOption = "conditionEnemyShocked", apply = function(val,modList,enemyModList)
