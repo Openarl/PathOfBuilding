b6d5d0984193b543ba687002044e2ed3d3807b5e
Quotae <quota97@gmail.com>
2019-09-30 19:32:59 -0500

    Revert "Merge pull request #1 from Quotae/elusive"
    This reverts commit 689b2fc37ed199bc17850bc521bfe11538fd24cc, reversing
    changes made to 39853e033da17a907d3ca29d1a40450a7e9f6c9f.

--- Data/3_0/Skills/sup_dex.lua
+++ Data/3_0/Skills/sup_dex.lua
@@ -1805,17 +1805,7 @@ skills["SupportPuncturingWeapon"] = {
 	ignoreMinionTypes = true,
 	statDescriptionScope = "gem_stat_descriptions",
 	baseMods = {
-		flag("Condition:Elusive"),
-		mod("Dummy", "DUMMY", 1, 0, 0, { type = "Condition", var = "Elusive" }),
 	},
-	statMap = {
-		["elusive_effect_+%"] = {
-			mod("ElusiveEffect", "INC", nil, 0, 0, { type = "GlobalEffect", effectType = "Buff" }),
-		},
-		["nightblade_elusive_grants_critical_strike_multiplier_+_to_supported_skills"] = {
-			mod("CritMultiplier", "BASE", nil, 0, 0, { type = "Condition", var = "Elusive" }),
-		},
-	},	
 	qualityStats = {
 		{ "critical_strike_chance_+%", 1 },
 	},
@@ -2825,14 +2815,8 @@ skills["SupportChaosAttacks"] = {
 	excludeSkillTypes = { },
 	statDescriptionScope = "gem_stat_descriptions",
 	baseMods = {
-		mod("ChaosDamageTaken", "INC", 6, 0, 0, { type = "GlobalEffect", effectType = "Debuff", effectName = "Withered", effectStackVar = "WitheredStackCount", effectStackLimit = 15 }),
+		mod("ChaosDamageTaken", "INC", 6, 0, 0, { type = "GlobalEffect", effectType = "Debuff", effectName = "Withered", effectStackVar = "WitheringTouchWitheredStackCount", effectStackLimit = 15 }),
 	},
-	statMap = {
-		["withered_on_hit_chance_%"] = {
-			flag("Condition:CanWither"),
-			mod("Dummy", "DUMMY", 1, 0, 0, { type = "Condition", var = "CanWither" }),
-		},
-	},	
 	qualityStats = {
 		{ "chaos_damage_+%", 0.5 },
 	},
